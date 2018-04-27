a519fb092c93d198a245ce940970b6b42eede268
AliAkbarMontazeri <aliakbarm534@gmail.com>
2018-04-27 19:07:48 +0700

    Changed for linux ubuntu 17 artfull gcc7

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "panducoin"; // padc coin
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001f8a181eff50b029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f47894a4709b4ea990c63038bad63d6b9c3cd77d46049622180308cb0ee12f47";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
